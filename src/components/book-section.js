9c4efafb51b19ad9b6f4f812e47f587ae8045af2
anas599 <anas599@yahoo.com>
2023-02-28 14:11:46 +0300

    change book section to display dynamically

--- src/components/book-section.js
+++ src/components/book-section.js
@@ -1,68 +1,51 @@
+import { useSelector } from 'react-redux';
 import TitleAuthor from './props/title-author';
 
 function BookSection() {
+  const booksArr2 = useSelector((state) => state.books.booksArray);
   return (
     <>
-      <section className="bookDiv">
-        <div className="book">
-          <TitleAuthor
-            category="Action"
-            name="The Hunger Games"
-            author="Suzanne Collins"
-            className="bookName"
-          />
-          <div>
-            <ul className="buttonsDiv">
-              <li><button type="button">Comments</button></li>
-              <li><button type="button">Remove</button></li>
-              <li><button type="button">Edit</button></li>
-            </ul>
+      {booksArr2.map((book) => (
+        <section className="bookDiv" key={book.id}>
+          <div className="book">
+            <TitleAuthor
+              id={book.item_id}
+              category={book.category}
+              name={book.title}
+              author={book.author}
+              className="bookName"
+            />
+            <div>
+              <ul className="buttonsDiv">
+                <li>
+                  <button type="button">Comments</button>
+                </li>
+                <li>
+                  <button type="button">Remove</button>
+                </li>
+                <li>
+                  <button type="button">Edit</button>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="completion">
-          <div className="circle" />
-          <div>
-            <h3>64%</h3>
-            <p>Completed</p>
+          <div className="completion">
+            <div className="circle" />
+            <div>
+              <h3>64%</h3>
+              <p>Completed</p>
+            </div>
           </div>
-        </div>
-        <div className="bar" />
-        <div className="progress">
-          <p>Current Chart</p>
-          <p>Chapter 17</p>
-          <button type="button" className="updateBtn">UPDATE PROGRESS</button>
-        </div>
-      </section>
-      <section className="bookDiv">
-        <div className="book">
-          <TitleAuthor
-            category="Science fiction"
-            name="Dune"
-            author="Frank Herbert"
-            className="bookName"
-          />
-          <div>
-            <ul className="buttonsDiv">
-              <li><button type="button">Comments</button></li>
-              <li><button type="button">Remove</button></li>
-              <li><button type="button">Edit</button></li>
-            </ul>
+          <div className="bar" />
+          <div className="progress">
+            <p>Current Chart</p>
+            <p>Chapter 17</p>
+            <button type="button" className="updateBtn">
+              UPDATE PROGRESS
+            </button>
           </div>
-        </div>
-        <div className="completion">
-          <div className="circle" />
-          <div>
-            <h3>64%</h3>
-            <p>Completed</p>
-          </div>
-        </div>
-        <div className="bar" />
-        <div className="progress">
-          <p>Current Chart</p>
-          <p>Chapter 17</p>
-          <button type="button" className="updateBtn">UPDATE PROGRESS</button>
-        </div>
-      </section>
+        </section>
+      ))}
       <section className="newBook">
         <h3>ADD NEW BOOK</h3>
         <form>
