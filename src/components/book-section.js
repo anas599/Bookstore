56842fb8c9b0743ba2d1f1217e89e1113d38cbf2
anas599 <anas599@yahoo.com>
2023-03-02 14:09:02 +0300

    Add reducer to display books from api using axios

--- src/components/book-section.js
+++ src/components/book-section.js
@@ -1,16 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import TitleAuthor from './props/title-author';
 import RemoveButton from './props/Buttons/removeButton';
-import { removeBook, addBook } from '../redux/books/booksSlice';
+import { removeBook, addBook, getBooksArr } from '../redux/books/booksSlice';
 import AddButton from './props/Buttons/addButton';
 
 function BookSection() {
-  const booksArr2 = useSelector((state) => state.books.booksArray);
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-
+  useEffect(() => {
+    dispatch(getBooksArr());
+  }, [dispatch]);
+  const booksArray = useSelector((state) => state.books.booksArray);
   const handleClick = (id) => {
     dispatch(removeBook(id));
   };
@@ -18,46 +20,39 @@ function BookSection() {
   const titleFunc = (event) => {
     setTitle(event.target.value);
   };
+
   const authorFunc = (event) => {
     setAuthor(event.target.value);
   };
 
-  const handleAddBook = (event) => {
-    event.preventDefault();
-    dispatch(
-      addBook({
-        title, author,
-      }),
-    );
-  };
-
   return (
     <>
-      {booksArr2.map((book) => (
-        <section className="bookDiv" key={book.item_id}>
+      {Object.values(booksArray).map((item) => item.map((book) => (
+        <section className="bookDiv" key={book.title}>
           <div className="book">
             <TitleAuthor
-              id={book.item_id}
+              key={book.category}
+              id={book.category}
               category={book.category}
-              name={book.title}
+              title={book.title}
               author={book.author}
               className="bookName"
             />
             <div>
               <ul className="buttonsDiv">
                 <li>
-                  <button type="button" id={book.item_id}>
+                  <button type="button" id={book.title}>
                     Comments
                   </button>
                 </li>
                 <li>
                   <RemoveButton
-                    id={book.item_id}
-                    onClick={() => handleClick(book.item_id)}
+                    id={book.title}
+                    onClick={() => handleClick(book.title)}
                   />
                 </li>
                 <li>
-                  <button type="button" id={book.item_id}>
+                  <button type="button" id={book.title}>
                     Edit
                   </button>
                 </li>
@@ -80,7 +75,7 @@ function BookSection() {
             </button>
           </div>
         </section>
-      ))}
+      )))}
       <section className="newBook">
         <h3>ADD NEW BOOK</h3>
         <form onSubmit={handleAddBook}>
@@ -96,11 +91,29 @@ function BookSection() {
             value={author}
             onChange={authorFunc}
           />
-          <AddButton id="newBook" title={title} author={author} category="N/A" />
+          <AddButton
+            id="newBook"
+            title={title}
+            author={author}
+            category="N/A"
+          />
         </form>
       </section>
     </>
   );
+
+  function handleAddBook(event) {
+    event.preventDefault();
+    dispatch(
+      addBook({
+        title,
+        author,
+        category: 'N/A',
+      }),
+    );
+    setTitle('');
+    setAuthor('');
+  }
 }
 
 export default BookSection;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,33 +1,74 @@
-import { createSlice } from '@reduxjs/toolkit';
-import booksArray2 from '../booksArr';
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+
+const apiBooks = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/zpVDBnBpLThUE35BUZY9/books';
+
+export const getBooksArr = createAsyncThunk(
+  'books/getBooksArr',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(apiBooks);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('Error...');
+    }
+  },
+);
+
+export const deleteBook = createAsyncThunk(
+  'books/deleteBook',
+  async (id, thunkAPI) => {
+    try {
+      await axios.delete(`${apiBooks}/${id}`);
+      return id;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('Book does not exist');
+    }
+  },
+);
+
+export const addBookApi = createAsyncThunk(
+  'books/addBook',
+  async (book, thunkAPI) => {
+    try {
+      const response = await axios.post(apiBooks, book);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('Error...');
+    }
+  },
+);
 
 export const booksSlice = createSlice({
   name: 'books',
   initialState: {
-    booksArray: [...booksArray2],
+    booksArray: [],
+    loading: false,
   },
-  reducers: {
-    addBook: (state, action) => {
-      const {
-        title, author, category,
-      } = action.payload;
-      const newAdd = {
-        item_id: `item${state.booksArray.length + 1}`,
-        title,
-        author,
-        category,
-      };
-      state.booksArray.push(newAdd);
+  extraReducers: {
+    [getBooksArr.pending]: (state) => {
+      state.loading = true;
     },
-    removeBook: (state, action) => {
-      const acp = action.payload;
-      // eslint-disable-next-line no-param-reassign
+    [getBooksArr.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.booksArray = action.payload;
+    },
+    [getBooksArr.rejected]: (state) => {
+      state.loading = false;
+    },
+    [deleteBook.fulfilled]: (state, action) => {
       state.booksArray = state.booksArray.filter(
-        (book) => book.item_id !== acp,
+        (book) => book.item_id !== action.payload,
       );
+    },
+    [addBookApi.fulfilled]: (state, action) => {
+      state.booksArray.push(action.payload);
     },
   },
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
+
 export default booksSlice.reducer;
